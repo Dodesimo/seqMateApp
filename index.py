171d8b47871c70a2d1d998e1d71ff5a5bc483501
Devam Mondal <devammondal06@gmail.com>
2024-06-20 17:22:43 -0400

    start backend

--- index.py
+++ index.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-
 def main():
     return render_template("index.html")
+
+@app.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
 
 if __name__ == '__main__':
     app.run()
